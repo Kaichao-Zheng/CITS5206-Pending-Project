df550d92da7f8e281a9d49448fc2e62965c9c94a
jordanrigden <jordanrigden11@gmail.com>
2025-08-09 17:36:00 +0800

    Added recursive searching of links to find information

--- gov_scraper.py
+++ gov_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from gov_scraper_person import Person
+import time
 
 # Function to parse the organisations from the response
 def parseOrganisations(response, element, elementClass):
@@ -14,46 +15,87 @@ def getPage(baseURL):
         return response
     else:
         print(f"Failed to fetch the page. Status code: {response.status_code}")
+        exit()
+
+#
+def parsePeople(organisation, personElement):
+    person_obj = Person()
+
+    person_obj.addOrganisation(organisation)
+
+    position_element = personElement.find("a")
+    person_position = position_element.text.strip()
+    person_obj.addPosition(person_position)
+
+    name_element = position_element.find_next("a")
+    person_name = name_element.text.strip()
+    person_obj.addName(person_name)
+
+    phone_element = personElement.find('a', attrs={"data-original-title": "Phone Number"})
+    if phone_element:
+        person_phone = phone_element.get_text(strip=True)
+        person_obj.addPhone(person_phone)
+                    
+    email_element = personElement.find('a', attrs={"data-original-title": "Email"})
+    if email_element:
+        person_email = email_element.get_text(strip=True)
+        person_obj.addEmail(person_email)
+
+    return person_obj
+
+# Finds the text within an element
+def findText(element, text):
+    return element.find(lambda tag: text in tag.get_text())
+
+# Parses the key people and prints their details
+def parseKeyPeople(element):
+    people = element.find_all("li", class_="list-group-item")
+    for person in people:
+        return parsePeople(organisation, person)
+    
+def findSubsectors(element, sector_name):
+    subsectors = element.find_all("li")
+    for subsector in subsectors:
+        if subsector.find("ul"):
+            inner_subsectors = subsector.find("ul").find_all("li")
+            next = BeautifulSoup(str(inner_subsectors), "html.parser")
+            findSubsectors(next, sector_name)
+        else:
+            a_tag = subsector.find("a")
+            if a_tag:
+                subsector_name = sector_name + ' - ' + subsector.text.strip()
+                subsector_href = a_tag["href"]
+                subsector_page = getPage(baseURL + subsector_href)
+                subsector_results = parseOrganisations(subsector_page, "section", ["views-element-container", "block-directory-custom"])
+                for subsector_result in subsector_results:
+                    if findText(subsector_result, "Key People"):
+                        person_obj = parseKeyPeople(subsector_result)
+                        person_obj.organisation = person_obj.organisation + subsector_name
+                        print(f"Name: {person_obj.name}, Organisation: {person_obj.organisation}, Position: {person_obj.position}, Phone: {person_obj.phone}, Email: {person_obj.email}")
 
 baseURL = 'https://www.directory.gov.au'
 page = getPage(baseURL + '/commonwealth-entities-and-companies')
 results = parseOrganisations(page, "td", "views-field views-field-title")
 
+#M Main loop to iterate through each organisation
 for result in results:
     a_tag = result.find("a")  # Find the <a> tag within the <td>
     if a_tag:
         organisation = result.text.strip()
         href = a_tag["href"]  # Extract the href attribute
         organisation_page = getPage(baseURL + href)
-        organisation_results = parseOrganisations(organisation_page, "section", "views-element-container")
+        organisation_results = parseOrganisations(organisation_page, "section", ["views-element-container", "block-directory-custom"])
         for organisation_result in organisation_results:
-            if organisation_result.find(lambda tag: "Key People" in tag.get_text()):
-                people = organisation_result.find_all("li", class_="list-group-item")
-                for person in people:
-                    person_obj = Person()
+            # ===========
+            # Checks for immediate people listed within the organisation
+            # ===========
+            if findText(organisation_result, "Key People"):
+                person_obj = parseKeyPeople(organisation_result)
+                print(f"Name: {person_obj.name}, Organisation: {person_obj.organisation}, Position: {person_obj.position}, Phone: {person_obj.phone}, Email: {person_obj.email}")
 
-                    person_obj.addOrganisation(organisation)
-
-                    position_element = person.find("a")
-                    person_position = position_element.text.strip()
-                    person_obj.addPosition(person_position)
-
-                    name_element = position_element.find_next("a")
-                    person_name = name_element.text.strip()
-                    person_obj.addName(person_name)
-
-                    phone_element = person.find('a', attrs={"data-original-title": "Phone Number"})
-                    if phone_element:
-                        person_phone = phone_element.get_text(strip=True)
-                        person_obj.addPhone(person_phone)
-                    
-                    email_element = person.find('a', attrs={"data-original-title": "Email"})
-                    if email_element:
-                        person_email = email_element.get_text(strip=True)
-                        person_obj.addEmail(person_email)
-
-                    
-                    # Print the person's details
-                    print(f"Name: {person_obj.name}, Organisation: {person_obj.organisation}, Position: {person_obj.position}, Phone: {person_obj.phone}, Email: {person_obj.email}")
-        
-
+            # ===========
+            # Checks for sub-sectors within the organisation
+            # ===========
+            if findText(organisation_result, "Sections"):
+                subsector_name = ""
+                findSubsectors(organisation_result, subsector_name)
